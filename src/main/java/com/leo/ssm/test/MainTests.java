e00f00f2eefe116677c4bb50b1973f64dc8ab34a
BruceLeo <1400061312@qq.com>
2021-05-06 15:16:31 +0800

    整合ssm框架

--- src/main/java/com/leo/ssm/test/MainTests.java
+++ src/main/java/com/leo/ssm/test/MainTests.java
@@ -3,6 +3,6 @@ package com.leo.ssm.test;
 public class MainTests {
     public static void main(String[] args) {
         System.out.println("git1");
-        System.out.println("git2");
+        System.out.println("git");
     }
 }
